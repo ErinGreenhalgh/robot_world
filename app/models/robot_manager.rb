97471a2338f22f8dc4600e50e20faaf9a1f91a81
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-05-14 13:29:06 -0600

    gitignore capybara save-and-open-page files

--- app/models/robot_manager.rb
+++ app/models/robot_manager.rb
@@ -33,18 +33,18 @@ class RobotManager
     Robot.new(raw_robot(id))
   end
 
-  # def update(id, robot_data)
-  #   database.transaction do
-  #     target_robot = database['robots'].find { |data| data["name"] == name }
-  #     target_robot["name"] = robot_data[:name]
-  #     target_robot["city"] = robot_data[:city]
-  #     target_robot["state"] = robot_data[:state]
-  #     target_robot["avatar"] = robot_data[:avatar]
-  #     target_robot["birthdate"] = robot_data[:birthdate]
-  #     target_robot["date_hired"] = robot_data[:date_hired]
-  #     target_robot["department"] = robot_data[:department]
-  #   end
-  # end
+  def update(id, robot_data)
+    database.transaction do
+      target_robot = database['robots'].find { |data| data["name"] == name }
+      target_robot["name"] = robot_data[:name]
+      target_robot["city"] = robot_data[:city]
+      target_robot["state"] = robot_data[:state]
+      target_robot["avatar"] = robot_data[:avatar]
+      target_robot["birthdate"] = robot_data[:birthdate]
+      target_robot["date_hired"] = robot_data[:date_hired]
+      target_robot["department"] = robot_data[:department]
+    end
+  end
 
   def delete_all
     table.delete
