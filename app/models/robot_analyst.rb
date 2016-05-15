71087dbf81e03a566b978014bc5a1aec840a715b
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-05-15 10:30:05 -0600

    model tests passing for finding all the states and the number of robots per state; but getting repetitive; need to refactor

--- app/models/robot_analyst.rb
+++ app/models/robot_analyst.rb
@@ -6,8 +6,7 @@ class RobotAnalyst
   end
 
   def collect_all_cities
-    robots = robot_manager.all
-    robots.map do |robot|
+    robot_manager.all.map do |robot|
       {robot.city => 1}
     end
   end
@@ -24,7 +23,27 @@ class RobotAnalyst
     end.to_h
   end
 
-  
+  def collect_all_states
+    robot_manager.all.map do |robot|
+      {robot.state => 1}
+    end
+  end
+
+  def group_by_state
+    collect_all_states.group_by do |hash|
+      hash.keys[0]
+    end
+  end
+
+  def robots_per_state
+    group_by_state.map do |key, value|
+      [key, value.count]
+    end.to_h
+  end
+
+
+
+
 
 
 
--- test/models/robot_analyst_test.rb
+++ test/models/robot_analyst_test.rb
@@ -3,12 +3,13 @@ require './test/test_helper'
 class RobotAnalystTest < Minitest::Test
   include TestHelper
 
-  def test_it_can_collect_all_the_cities
+  def setup
     data1 =
     {
       name: "Henry",
       city: "Denver",
       state: "CO",
+      department: "IT"
     }
 
     data2=
@@ -16,6 +17,7 @@ class RobotAnalystTest < Minitest::Test
       name: "Henrietta",
       city: "Tucson",
       state: "AZ",
+      department: "HR"
     }
 
     data3=
@@ -23,85 +25,60 @@ class RobotAnalystTest < Minitest::Test
       name: "Pheobe",
       city: "Tucson",
       state: "AZ",
+      department: "IT"
     }
 
     robot_manager.create(data1)
     robot_manager.create(data2)
     robot_manager.create(data3)
 
-    ra = RobotAnalyst.new(robot_manager)
+    @ra = RobotAnalyst.new(robot_manager)
+  end
+
+  def test_it_can_collect_all_the_cities
     collected = [{"Denver"=>1}, {"Tucson"=>1}, {"Tucson"=>1}]
-    assert_equal collected, ra.collect_all_cities
+    assert_equal collected, @ra.collect_all_cities
   end
 
   def test_it_can_group_by_city
-    data1 =
-    {
-      name: "Henry",
-      city: "Denver",
-      state: "CO",
-    }
-
-    data2=
-    {
-      name: "Henrietta",
-      city: "Tucson",
-      state: "AZ",
-    }
-
-    data3=
-    {
-      name: "Pheobe",
-      city: "Tucson",
-      state: "AZ",
-    }
-
-    robot_manager.create(data1)
-    robot_manager.create(data2)
-    robot_manager.create(data3)
-
-    ra = RobotAnalyst.new(robot_manager)
     grouped =
     {
       "Denver" => [{"Denver" => 1}],
       "Tucson" => [{"Tucson" => 1}, {"Tucson" => 1}]
     }
-    assert_equal grouped, ra.group_by_city
+    assert_equal grouped, @ra.group_by_city
   end
 
   def test_it_can_give_robots_per_city
-    data1 =
-    {
-      name: "Henry",
-      city: "Denver",
-      state: "CO",
-    }
-
-    data2=
-    {
-      name: "Henrietta",
-      city: "Tucson",
-      state: "AZ",
-    }
-
-    data3=
-    {
-      name: "Pheobe",
-      city: "Tucson",
-      state: "AZ",
-    }
-
-    robot_manager.create(data1)
-    robot_manager.create(data2)
-    robot_manager.create(data3)
-
     city_data =
     {
       "Denver" => 1,
       "Tucson" => 2
     }
-    ra = RobotAnalyst.new(robot_manager)
-    assert_equal city_data, ra.robots_per_city
+    assert_equal city_data, @ra.robots_per_city
+  end
+
+  def test_it_can_collect_all_states
+    collected = [{"CO"=>1}, {"AZ"=>1}, {"AZ"=>1}]
+    assert_equal collected, @ra.collect_all_states
+  end
+
+  def test_it_can_group_all_states
+    grouped =
+    {
+      "CO" => [{"CO" => 1}],
+      "AZ" => [{"AZ" => 1}, {"AZ" => 1}]
+    }
+    assert_equal grouped, @ra.group_by_state
+  end
+
+  def test_it_can_give_robots_per_state
+    state_data =
+    {
+      "CO" => 1,
+      "AZ" => 2
+    }
+    assert_equal state_data, @ra.robots_per_state
   end
 
 end
